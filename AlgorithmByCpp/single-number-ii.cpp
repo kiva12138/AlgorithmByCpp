32346a582bf46901cbbdd1d22e2560719b71f49d
kiva12138 <sunhaoxx@foxmail.com>
2020-10-21 14:49:17 +0800

    Single Number

--- AlgorithmByCpp/single-number-ii.cpp
+++ AlgorithmByCpp/single-number-ii.cpp
@@ -9,7 +9,12 @@ public:
 
     // 只有一个数字出现一次，其余三次
     int singleNumber(vector<int>& nums) {
-
+        int once = 0, twice = 0;
+        for (auto x : nums) {
+            once = (once ^ x) & (~twice);
+            twice = (twice ^ x) & (~once);
+        }
+        return once;
     }
 
     void test() {
--- AlgorithmByCpp/single-number.cpp
+++ AlgorithmByCpp/single-number.cpp
@@ -9,10 +9,15 @@ public:
 
     // 只有一个数字出现一次，其余2次
     int singleNumber(vector<int>& nums) {
-
+        int result = 0;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
+            result = result ^ *it;
+        }
+        return result;
     }
 
     void test() {
-
+        vector<int> nums = { 2,2,3 };
+        cout << singleNumber(nums);
     }
 };
